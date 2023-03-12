ecc692874f82ac1729cd3ac1ce64bf493d1697ba
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-03-12 14:40:51 +0600

    add API

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const loadAIData = async () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayAIData(data.data);
+}
+
+const displayAIData = aidatas => {
+    const aiContainer = document.getElementById('ai-container');
+    aidatas.forEach(aidata => {
+        const aidataDiv = document.createElement('div');
+        aidataDiv.classList.add('col');
+        aidataDiv.innerHTML = `
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                                    to additional
+                                    content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+        `;
+        aiContainer.appendChild(dataDiv);
+
+    })
+}
+
+loadAIData();
