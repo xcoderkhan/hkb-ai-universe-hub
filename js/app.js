166975b735dd6489c4a6980fe717d9c63f121fea
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-03-12 19:35:35 +0600

    FetchData update

--- js/app.js
+++ js/app.js
@@ -1,29 +1,28 @@
 const loadAIData = async () => {
-    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
     const res = await fetch(url);
     const data = await res.json();
-    displayAIData(data.data);
+    displayAIData(data.data.tools);
 }
 
 const displayAIData = aidatas => {
     const aiContainer = document.getElementById('ai-container');
     aidatas.forEach(aidata => {
-        const aidataDiv = document.createElement('div');
-        aidataDiv.classList.add('col');
-        aidataDiv.innerHTML = `
-                        <div class="card">
-                            <img src="..." class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                    to additional
-                                    content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-        `;
+        const dataDiv = document.createElement('div');
+        dataDiv.classList.add('col');
+        dataDiv.innerHTML = `
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                        to additional
+                        content. This content is a little bit longer.</p>
+                </div>
+        </div>`;
         aiContainer.appendChild(dataDiv);
 
-    })
+    });
 }
 
 loadAIData();
